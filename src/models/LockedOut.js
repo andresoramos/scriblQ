41f90eb3e39716a5e520b09ed30442b9cbfd25c5
Andres Ramos <andresoramos@gmail.com>
2020-06-17 22:21:32 +0100

    Fixed moving window for IP lockout

--- src/models/LockedOut.js
+++ src/models/LockedOut.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 const Joi = require("@hapi/joi");
-const jwt = require("jsonwebtoken");
-const config = require("config");
 const _ = require("lodash");
 
 const userSchema = new mongoose.Schema({
@@ -14,10 +12,26 @@ const userSchema = new mongoose.Schema({
 function validateUser(ip) {
   const schema = Joi.object({
     ips: Joi.object().required(),
+    _id: Joi.string().max(1000).required(),
+    __v: Joi.number().required(),
   });
 
   return ({ error, value } = schema.validate(ip));
 }
 
+function validatePost(ip) {
+  const schema = Joi.object({
+    ips: Joi.object().required(),
+  });
+  return ({ error, value } = schema.validate(ip));
+}
+
+function validateBannedFix(ip) {
+  const schema = Joi.object({
+    duplicateArr: Joi.array().required(),
+  });
+  return ({ error, value } = schema.validate(ip));
+}
+
 const LockedOut = mongoose.model("LockedOut", userSchema);
-module.exports = { LockedOut, validateUser };
+module.exports = { LockedOut, validateUser, validatePost, validateBannedFix };
--- src/routes/lockedOut.js
+++ src/routes/lockedOut.js
@@ -1,6 +1,30 @@
 const express = require("express");
 const lockedOutRouter = express.Router();
-const { LockedOut, validateUser } = require("../models/LockedOut");
+const {
+  LockedOut,
+  validateUser,
+  validatePost,
+  validateBannedFix,
+} = require("../models/LockedOut");
+
+lockedOutRouter.put("/expiredBans/:id", async (req, res) => {
+  const { error } = validateBannedFix(req.body);
+  if (error) {
+    console.log(error.details[0].message);
+    return res.status(400).send(error.details[0].message);
+  }
+  const id = req.params.id;
+  const lockedOut = await LockedOut.findById(id);
+  const newLockedOut = { ...lockedOut._doc.ips };
+  newLockedOut.banned = req.body.duplicateArr;
+  const updated = await LockedOut.update(
+    { _id: req.params.id },
+    { $set: { ips: newLockedOut } }
+  );
+  if (updated) {
+    return res.send({ response: updated });
+  }
+});
 
 lockedOutRouter.put("/:id", async (req, res) => {
   const { error } = validateUser(req.body);
@@ -19,13 +43,14 @@ lockedOutRouter.put("/:id", async (req, res) => {
 });
 
 lockedOutRouter.post("/", async (req, res) => {
-  const { error } = validateUser(req.body);
+  const { error } = validatePost(req.body);
   if (error) {
     console.log(error.details[0].message);
     return res.status(400).send(error.details[0].message);
   }
-
-  const lockedOut = new LockedOut({ ips: req.body.ips });
+  const banned = { ...req.body.ips };
+  banned.banned = [];
+  const lockedOut = new LockedOut({ ips: banned });
   lockedOut.save();
   res.send(lockedOut);
 });
